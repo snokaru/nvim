5e49ca294402d2a41cf7a98d6bba8dd3c0576d30
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-01-10 09:32:17 +0200

    Initial commit

---
+++ lua/default/keymaps.lua
@@ -0,0 +1,50 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- explorer
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- resizing with arrows (doesn't work on mac)
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- navigating buffers with shift
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- jk to fast enter (?)
+keymap("i", "jk", "<ESC>", opts)
+
+-- hold onto visual mode after indenting
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- pasting without yanking
+keymap("v", "p", '"_dP', opts)
+
+-- use alt key to move text in visual mode (doesn't work on mac)
+-- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+-- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
