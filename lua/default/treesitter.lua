d11e310b30fcac32a9b20b892092e6c07eee9774
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-06-12 23:40:45 +0300

    Remove phpdoc dependency from treesitter to prevent error

--- lua/default/treesitter.lua
+++ lua/default/treesitter.lua
@@ -2,7 +2,7 @@ local configs = require("nvim-treesitter.configs")
 configs.setup {
   ensure_installed = "all",
   sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 
   -- for autopairs plugin
   autopairs = {
